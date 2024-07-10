07d6afd05a5bcea56a8aa76da08f4d61ed5e41c2
Desmond McCarthy <desmac96@icloud.com>
2024-07-10 08:30:51 +0000

    improve script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,74 @@
 console.log("questions.js has been loaded successfully.");
 
-
 // Questions array imported from questions.js //
-import {
-    quizQuestions
-} from "./questions.js";
+import { quizQuestions } from "./questions.js";
 
-const quiz = document.getElementById('quiz-app')
-const answerOpt = document.querySelectorAll('.answer')
-const questionOpt = document.getElementById('questionaire')
-const a_text = document.getElementById('a_answer')
-const b_text = document.getElementById('b_answer')
-const c_text = document.getElementById('c_answer')
-const d_text = document.getElementById('d_answer')
-const submitBtn = document.getElementById('submit')
-submitBtn.addEventListener("click", loadNextQuestion);
-let currentQuestion = 0
-let score = 0
-loadQuestion()
+const quiz = document.getElementById('quiz-app');
+const answerOpt = document.querySelectorAll('.answer');
+const questionOpt = document.getElementById('questionaire');
+const a_text = document.getElementById('a_answer');
+const b_text = document.getElementById('b_answer');
+const c_text = document.getElementById('c_answer');
+const d_text = document.getElementById('d_answer');
+const submitBtn = document.getElementById('submit');
 
-function loadNextQuestion() {
-    currentQuestion = currentQuestion + 1
-    loadQuestion();
-}
+let currentQuestionIndex = 0;
+let score = 0;
+
+submitBtn.addEventListener("click", handleAnswerSubmission);
 
 function loadQuestion() {
-    deselectAnswers()
-    const currentQuestionData = quizQuestions[currentQuestion]
-    questionOpt.innerText = currentQuestionData.question
-    a_text.innerText = currentQuestionData.a
-    b_text.innerText = currentQuestionData.b
-    c_text.innerText = currentQuestionData.c
+    deselectAnswers();
+    const currentQuestionData = quizQuestions[currentQuestionIndex];
+    questionOpt.innerText = currentQuestionData.question;
+    a_text.innerText = currentQuestionData.a;
+    b_text.innerText = currentQuestionData.b;
+    c_text.innerText = currentQuestionData.c;
     d_text.innerText = currentQuestionData.d;
 }
 
 function deselectAnswers() {
-    answerOpt.forEach(answerEL => answerEL.checked = false)
+    answerOpt.forEach(answerEl => answerEl.checked = false);
 }
 
 function getSelectedOpt() {
-    let answer
-    answerOpt.forEach(answerEL => {
-        if (answerEL.checked) {
-            answer = answerEL
+    let selectedAnswer;
+    answerOpt.forEach(answerEl => {
+        if (answerEl.checked) {
+            selectedAnswer = answerEl.id;
         }
-    })
+    });
+    return selectedAnswer;
 }
 
-function checkAnswer() {
-
-    if (this.id == myQuestions[currentQuestionIndex].correctAnswer) {
-        this.classList.add("correct");
-        incrementScore();
+function handleAnswerSubmission() {
+    const selectedAnswer = getSelectedOpt();
+    if (selectedAnswer) {
+        checkAnswer(selectedAnswer);
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizQuestions.length) {
+            loadQuestion();
+        } else {
+            displayResults();
+        }
     } else {
-        this.classList.add("incorrect");
+        alert("Please select an answer before proceeding.");
     }
-
-    setTimeout(() => {
-        this.classList.remove("correct");
-        this.classList.remove("incorrect");
-        setNextQuestion();
-    }, 1000);
-    currentQuestionIndex++;
 }
+
+function checkAnswer(selectedAnswer) {
+    if (selectedAnswer === quizQuestions[currentQuestionIndex].correctAnswer) {
+        incrementScore();
+    }
+}
+
+function incrementScore() {
+    score++;
+}
+
+function displayResults() {
+    quiz.innerHTML = `<h2>You scored ${score} out of ${quizQuestions.length}</h2>`;
+}
+
+// Initial call to load the first question
+loadQuestion();
